eb5901e49e7ea0756c7123c656311d56432bc954
y-temp4 <y.temp4@gmail.com>
2020-10-25 22:49:03 +0900

    Add meta settings

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,22 +5,43 @@ import Head from 'next/head'
 type Props = {
   children?: ReactNode
   title?: string
+  description?: string
+  pagePath?: string
 }
 
-const Layout = ({ children, title = `y-temp4's Diary` }: Props) => (
-  <>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header className="text-center mt-10 mb-12">
-      <h1 className="font-bold text-2xl">
-        <Link href="/">y-temp4's Diary</Link>
-      </h1>
-    </header>
-    <main className="max-w-screen-sm mx-auto mb-10 px-3">{children}</main>
-  </>
-)
-
+const Layout = ({
+  children,
+  title = `y-temp4's Diary`,
+  description = `y-temp4's English Diary.`,
+  pagePath = '',
+}: Props) => {
+  const baseURL = 'https://diary.y-temp4.com/'
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="blog" />
+        <meta property="og:url" content={`${baseURL}${pagePath}`} />
+        <meta property="og:image" content={`${baseURL}ogp.jpg`} />
+        <meta property="og:site_name" content={title} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content={`${baseURL}ogp.jpg`} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={`${baseURL}ogp.jpg`} />
+        <link rel="canonical" href={`${baseURL}${pagePath}`} />
+      </Head>
+      <header className="text-center mt-10 mb-12">
+        <h1 className="font-bold text-2xl">
+          <Link href="/">y-temp4's Diary</Link>
+        </h1>
+      </header>
+      <main className="max-w-screen-sm mx-auto mb-10 px-3">{children}</main>
+    </>
+  )
+}
 export default Layout
