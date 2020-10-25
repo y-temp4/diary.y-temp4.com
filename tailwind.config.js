45e794b235f1dc0265812e5d1d1c89db7758630f
y-temp4 <y.temp4@gmail.com>
2020-10-25 21:49:27 +0900

    Fix purge files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ['./components/**/*.jsx', './pages/**/*.jsx'],
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
