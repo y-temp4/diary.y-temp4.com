19bdd01a641dc98dbc045197c6d3e1e3b1d6bbbc
y-temp4 <y.temp4@gmail.com>
2020-10-27 18:43:53 +0900

    Fix to add sitemap encoding

--- lib/sitemap.ts
+++ lib/sitemap.ts
@@ -17,6 +17,7 @@ const generateCategorySitemapItem = (category: string) => `
 `
 
 export const generateSitemap = (posts: Post[], categories: string[]) => `
+<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${baseURL}</loc>
